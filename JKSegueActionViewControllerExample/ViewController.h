7f15ce7fecc8c419dd0a25370d743f1aec94f093
Joseph Kain <joekain@gmail.com>
2013-05-03 20:16:17 -0700

    Example App and tests

---
+++ JKSegueActionViewControllerExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JKSegueActionViewControllerExample
+//
+//  Created by Joseph Kain on 5/3/13.
+//  Copyright (c) 2013 Joseph Kain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ JKSegueActionViewControllerExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JKSegueActionViewControllerExample
+//
+//  Created by Joseph Kain on 5/3/13.
+//  Copyright (c) 2013 Joseph Kain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
